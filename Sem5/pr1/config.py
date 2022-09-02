a8b4233389cf6c40b9f89124d4cdcd3001fe9d93
Vitaliy <svitalik2002@gmail.com>
2022-09-02 19:59:09 +0300

    API pr1 hide token try2

--- Sem5/pr1/config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': 'MTAxNDg5NjY1Mzg0MjUyMjE2Mg.GjCAdh.1Sno9n9aJqI4uio9B8h--gi7p0Ou58PR3k8O84',
-    'bot': 'TestingBot11111',
-    'id': 1014896653842522162,
-    'prefix': '!'
-}
